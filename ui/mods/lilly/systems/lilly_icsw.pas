8270dd5b3bbe4b58bad074c5c8148a5bf21002df
lilly <lilly1987@naver.com>
2022-11-07 19:17:09 +0900

    ...

---
+++ ui/mods/lilly/systems/lilly_icsw.pas
@@ -0,0 +1,42 @@
+{
+    "name": "lilly icSW",
+    "description": "",
+    "creator": "",
+    "version": "",
+    "planets": [
+        {
+            "name": "Pandaria",
+            "mass": 10000,
+            "position_x": 198019.203125,
+            "position_y": -488.5342102050781,
+            "velocity_x": 0.12396791577339172,
+            "velocity_y": 50.24922180175781,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "respawn": false,
+            "start_destroyed": false,
+            "min_spawn_delay": 0,
+            "max_spawn_delay": 0,
+            "planet": {
+                "seed": 1423044736,
+                "radius": 250,
+                "heightRange": 100,
+                "waterHeight": 70,
+                "waterDepth": 100,
+                "temperature": 0,
+                "metalDensity": 0,
+                "metalClusters": 100,
+                "metalSpotLimit": -1,
+                "biomeScale": 50,
+                "biome": "earth",
+                "symmetryType": "none",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 10,
+                "landingZonesPerArmy": 5,
+                "landingZoneSize": 500,
+                "heightAdjustments": []
+            }
+        }
+    ]
+}
