995a72ed4a3f13a5fe3cc7d783f93744e7b80211
lilly <lilly1987@naver.com>
2022-11-07 16:58:11 +0900

    ...

---
+++ ui/mods/lilly/systems/lilly_trbw.pas
@@ -0,0 +1,42 @@
+{
+    "name": "Lilly TrBW",
+    "description": "",
+    "creator": "",
+    "version": "",
+    "planets": [
+        {
+            "name": "C.A.J,C. Prime b w",
+            "mass": 50000,
+            "position_x": 12248.0478515625,
+            "position_y": 3062.011962890625,
+            "velocity_x": -48.26640319824219,
+            "velocity_y": 193.0655975341797,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "respawn": false,
+            "start_destroyed": false,
+            "min_spawn_delay": 0,
+            "max_spawn_delay": 0,
+            "planet": {
+                "seed": 699189632,
+                "radius": 1300,
+                "heightRange": 100,
+                "waterHeight": 70,
+                "waterDepth": 100,
+                "temperature": 80,
+                "metalDensity": 0,
+                "metalClusters": 100,
+                "metalSpotLimit": -1,
+                "biomeScale": 50,
+                "biome": "tropical",
+                "symmetryType": "none",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 2,
+                "landingZonesPerArmy": 5,
+                "landingZoneSize": 500,
+                "heightAdjustments": []
+            }
+        }
+    ]
+}
